33fa6e046ee96b786bfef4416d2b179952210f19
TekuConcept <electro_madness@yahoo.com>
2016-03-06 19:09:32 -0700

    Initial Project Setup
    All project independent driver files were copied from Poseidon to this
    repository and a general file structure was created.

---
+++ Src/Sensors/HMC5883L.cpp
@@ -0,0 +1,110 @@
+/*
+ * HMC5883L.cpp
+ *
+ *  Created on: Jan 31, 2015
+ * Last Edited: Feb  7, 2016
+ *      Author: TekuConcept
+ */
+
+#include "HMC5883L.h"
+
+
+HMC5883L::HMC5883L() : currentGain(Gain::G1_3) {}
+
+
+
+HMC5883L::~HMC5883L() {}
+
+
+
+float HMC5883L::X() {
+    return scaleWithGain(I2C::readShort(HMC5883L_ADDR, HMC5883L_X_H));
+}
+
+
+
+float HMC5883L::Y() {
+    return scaleWithGain(I2C::readShort(HMC5883L_ADDR, HMC5883L_Y_H));
+}
+
+
+
+float HMC5883L::Z() {
+    return scaleWithGain(I2C::readShort(HMC5883L_ADDR, HMC5883L_Z_H));
+}
+
+
+
+float HMC5883L::scaleWithGain(short value) {
+    return value * (1.0 / GAIN_SCALER[static_cast<int>(currentGain)]);
+}
+
+
+
+bool HMC5883L::isLocked() {
+    return (I2C::readByte(HMC5883L_ADDR, HMC5883L_STATUS) & 2) >> 1;
+}
+
+
+
+bool HMC5883L::isReady() {
+    return (I2C::readByte(HMC5883L_ADDR, HMC5883L_STATUS) & 1);
+}
+
+
+
+void HMC5883L::setSampleAverage(Sample ma) {
+    uint cra = I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_A) & 0x9F;
+    cra |= static_cast<int>(ma) << 5;
+    I2C::writeByte(HMC5883L_ADDR, HMC5883L_CONFIG_A, cra);
+}
+
+
+
+void HMC5883L::setOutputRate(Rate dor) {
+    uint cra = I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_A) & 0xE3;
+    cra |= static_cast<int>(dor) << 2;
+    I2C::writeByte(HMC5883L_ADDR, HMC5883L_CONFIG_A, cra);
+}
+
+
+
+void HMC5883L::setGain(Gain gn) {
+    currentGain = gn;
+    uint crb = I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_B) & 0x1F;
+    crb |= static_cast<int>(gn) << 5;
+    I2C::writeByte(HMC5883L_ADDR, HMC5883L_CONFIG_B, crb);
+}
+
+
+
+void HMC5883L::setMode(Mode md) {
+    int _m_ = static_cast<int>(md);
+    I2C::writeByte(HMC5883L_ADDR, HMC5883L_MODE, _m_);
+}
+
+
+
+int HMC5883L::getSampleAverage() {
+    return ((I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_A) & 0x60) >> 5);
+}
+
+
+
+int HMC5883L::getOutputRate() {
+    return ((I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_A) & 0x1C) >> 2);
+}
+
+
+
+int HMC5883L::getGain() {
+    int val = ((I2C::readByte(HMC5883L_ADDR, HMC5883L_CONFIG_B) & 0xE0) >> 5);
+    currentGain = static_cast<Gain>(val);
+    return val;
+}
+
+
+
+int HMC5883L::getMode() {
+    return ((I2C::readByte(HMC5883L_ADDR, HMC5883L_MODE) & 0x03));
+}
---
+++ Src/Sensors/MPU6050.cpp
@@ -0,0 +1,98 @@
+/*
+ * MPU6050.cpp
+ *
+ *  Created on: Feb 2, 2015
+ * Last Edited: Feb 7, 2016
+ *      Author: TekuConcept
+ */
+
+#include "MPU6050.h"
+
+
+MPU6050::MPU6050() : g_full(FS_SEL::FSG_250), a_full(AFS_SEL::FSA_2) {}
+
+
+
+MPU6050::~MPU6050() {}
+
+
+
+float MPU6050::accel_X() {
+    return scaleAccel(I2C::readShort(MPU6050_ADDR_A, MPU6050_ACCEL_XOUT_H));
+}
+
+
+
+float MPU6050::accel_Y() {
+    return scaleAccel(I2C::readShort(MPU6050_ADDR_A, MPU6050_ACCEL_YOUT_H));
+}
+
+
+
+float MPU6050::accel_Z() {
+    return scaleAccel(I2C::readShort(MPU6050_ADDR_A, MPU6050_ACCEL_ZOUT_H));
+}
+
+
+
+float MPU6050::gyro_X(){
+    return scaleGyro(I2C::readShort(MPU6050_ADDR_A, MPU6050_GYRO_XOUT_H));
+}
+
+
+
+float MPU6050::gyro_Y() {
+    return scaleGyro(I2C::readShort(MPU6050_ADDR_A, MPU6050_GYRO_YOUT_H));
+}
+
+
+
+float MPU6050::gyro_Z() {
+    return scaleGyro(I2C::readShort(MPU6050_ADDR_A, MPU6050_GYRO_ZOUT_H));
+}
+
+
+
+float MPU6050::temperature() {
+    int16_t v = 0;
+    v = I2C::readShort(MPU6050_ADDR_A, MPU6050_TEMP_OUT_H);
+    return (v / 340) + 36.53F;
+}
+
+
+
+float MPU6050::scaleGyro(short value){
+    return value * (1.0 / GYRO_SCALER[static_cast<int>(g_full)]);
+}
+
+
+
+float MPU6050::scaleAccel(short value) {
+    return value * (1.0 / ACCEL_SCALER[static_cast<int>(a_full)]);
+}
+
+
+
+void MPU6050::setGyroFullScale(FS_SEL select) {
+    g_full = select;
+    I2C::writeByte(MPU6050_ADDR_A, MPU6050_GYRO_CONFIG, static_cast<int>(select) << 3);
+}
+
+
+
+void MPU6050::setAcclFullScale(AFS_SEL select) {
+    a_full = select;
+    I2C::writeByte(MPU6050_ADDR_A, MPU6050_ACCEL_CONFIG, static_cast<int>(select) << 3);
+}
+
+
+
+void MPU6050::sleep() {
+    I2C::writeByte(MPU6050_ADDR_A, MPU6050_PWR_MGMT_1, 0x40);
+}
+
+
+
+void MPU6050::awake() {
+    I2C::writeByte(MPU6050_ADDR_A, MPU6050_PWR_MGMT_1, 0x00);
+}
